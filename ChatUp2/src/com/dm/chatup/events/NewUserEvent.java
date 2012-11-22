d72984d80d476e23a1b5b38c6c285643d39fa0c0
Daniel Mueller <dm.mueller1@gmx.de>
2012-11-22 12:41:06 +0100

    Sicherung 4
    Export der Serverlogik in eine eigene Bibliothek,
    Umbau  des Chats durch Einbinden der Library

--- ChatUp2/src/com/dm/chatup/events/NewUserEvent.java
+++
@@ -1,9 +0,0 @@
-package com.dm.chatup.events;
-
-import com.dm.chatup.chat.Contact;
-
-public interface NewUserEvent {
-	
-	public void reactOnNewUser(Contact c);
-
-}
--- ChatUp2/src/com/dm/chatup/events/NewUserInChatEvent.java
+++
@@ -1,9 +0,0 @@
-package com.dm.chatup.events;
-
-import com.dm.chatup.chat.Contact;
-
-public interface NewUserInChatEvent {
-	
-	public void reactOnNewUserInChat(Contact c, int userID);
-
-}
